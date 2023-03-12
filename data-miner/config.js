0728d8ed153046db49d1b49d1f6cec493991f566
🐸 <dice-a6db@outlook.com>
2023-03-12 23:16:11 +0000

    fix: shift config pos

--- data-miner/config.js
+++ data-miner/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   MONGO_URL: "mongodb://admin:admin@127.0.0.1:27017",
   DB_NAME: "me_db",
+  // FILES: "./tables/{Airbases,Terrains}.lua", // Run once per theatre
   FILES: "./tables/*.lua", // Run once per dcs version
   VIEWS: "./views/*.js", // Run once per dcs version
-  // FILES: "./tables/{Airbases,Terrains}.lua", // Run once per theatre
   ENVS: {
     MISSION: "http://127.0.0.1:12080/",
     GUI: "http://127.0.0.1:12081/",
